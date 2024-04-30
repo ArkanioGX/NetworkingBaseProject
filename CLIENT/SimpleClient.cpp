2ba8c190289251e108979aacc841bac3c8bd7fbb
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-04-30 13:05:58 +0200

    Send All Client Join Notifications

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <raylib.h>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -15,9 +16,21 @@ struct Message
 	string content;
 };
 
+enum infoType {Unknown, iNickname, iMessage};
+
+const std::string DELIMITER = "/|||/";
+
+void add(std::string* sOut, std::string s2);
+std::string parse(std::string s1, int infPos);
+infoType getInfoType(std::string s1);
+
+std::unordered_map<std::string, infoType> const infoTable = { 
+	{"nn",infoType::iNickname},
+	{"msg",infoType::iMessage}};
+
 int main(int argc, char* argv[]) {
 
-	vector<Message> log{Message{"ChArtFX", "Waiting for someone to talk to..."}};
+	vector<Message> log{};
 
 	if (SDLNet_Init() == -1) {
 		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
@@ -45,9 +58,9 @@ int main(int argc, char* argv[]) {
 	cin.clear();
 	cout << endl;
 
-	IPaddress ip = {stoi(ipStr)};
+	IPaddress ip;
 
-	if (SDLNet_ResolveHost(&ip, "localhost", port) == -1) {
+	if (SDLNet_ResolveHost(&ip, ipStr.c_str(), port) == -1) {
 		cerr << "Resolve Host error: " << SDLNet_GetError() << endl;
 		SDLNet_Quit();
 		return 1;
@@ -58,6 +71,20 @@ int main(int argc, char* argv[]) {
 		cerr << "TCP Open error: " << SDLNet_GetError() << endl;
 		SDLNet_Quit();
 		return 1;
+	}
+
+	//Send nickname Info
+	{
+		string nnSend;
+		add(&nnSend, "nn");
+		add(&nnSend, nickname);
+		int bytesSent = SDLNet_TCP_Send(clientSocket, nnSend.c_str(), nnSend.length() + 1);
+		if (bytesSent < nnSend.length() + 1) {
+			cerr << "SDLNet TCP Send error: " << SDLNet_GetError() << endl;
+			SDLNet_TCP_Close(clientSocket);
+			SDLNet_Quit();
+			return 1;
+		}
 	}
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first chat window!");
@@ -71,17 +98,19 @@ int main(int argc, char* argv[]) {
 		ClearBackground(GRAY);
 		DrawText("Welcome to ChArtFX!", 220, 15, 25, WHITE);
 		DrawRectangle(20, 50, SCREEN_WIDTH - 40, SCREEN_HEIGHT - 150, DARKGRAY);
-
+		 /*
 		char buffer[1024];
 		int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+		std::string bf = std::string(buffer);
 		if (bytesRead > 0) {
 			
-			char content[128] = { 0 };
-			char nick[128] = { 0 };
-
-			sscanf_s(buffer, "%s" "%s", nick, content);
-			log.push_back(Message{std::string(nick),std::string(content)});
-		}
+			if (getInfoType(bf) == infoType::iMessage) {
+				Message m;
+				m.nickname = parse(bf, 1);
+				m.content = parse(bf, 2);
+				log.push_back(m);
+			}
+		}*/
 
 		for (int msg = 0; msg < log.size(); msg++)
 		{
@@ -125,6 +154,33 @@ int main(int argc, char* argv[]) {
 
 
 	return 0;
+}
+
+void add(std::string* sOut, std::string s2) {
+	sOut->append(s2 + DELIMITER);
+}
+
+std::string parse(std::string s1, int infPos) {
+	size_t pos = 0;
+	int CurrentInfPos = 0;
+	std::string token;
+	while ((pos = s1.find(DELIMITER)) != std::string::npos) {
+		token = s1.substr(0, pos);
+		if (CurrentInfPos == infPos) {
+			return token;
+		}
+		CurrentInfPos++;
+		s1.erase(0, pos + DELIMITER.length());
+	}
+	return "";
+}
+
+infoType getInfoType(std::string s1) {
+	auto it = infoTable.find(parse(s1,0));
+	if (it != infoTable.end()) {
+		return it->second;
+	}
+	return infoType::Unknown;
 }
 
 
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -1,7 +1,8 @@
 #include <SDL.h>
 #include <SDL_net.h>
 #include <iostream>
-#include <map>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 struct Message
@@ -10,6 +11,29 @@ struct Message
 	string content;
 };
 
+struct User {
+	string nickname;
+	TCPsocket socket;
+
+	inline bool operator==(const TCPsocket tcp) {
+		return socket == tcp;
+	}
+
+};
+
+enum infoType { Unknown, iNickname, iMessage };
+
+const std::string DELIMITER = "/|||/";
+
+void add(std::string* sOut, std::string s2);
+std::string parse(std::string s1, int infPos);
+infoType getInfoType(std::string s1);
+void Send(TCPsocket tcp, std::string s);
+std::string Receive(TCPsocket tcp, bool pause);
+
+std::unordered_map<std::string, infoType> const infoTable = {
+	{"nn",infoType::iNickname},
+	{"msg",infoType::iMessage} };
 
 int main(int argc, char* argv[]) {
 
@@ -35,7 +59,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	std::map<TCPsocket, string> userList;
+	std::vector<User*> userList;
 
 	TCPsocket clientSocket;
 	while (true) {
@@ -43,16 +67,25 @@ int main(int argc, char* argv[]) {
 		if (clientSocket) {
 			char buffer[1024];
 
-			if (userList[clientSocket].empty()) {
-				cout << "A client reached the server!" << endl;
-				Message message = Message{ "ChArtFX", "A client reached the server!" };
-				char buffer[1024];
-				int len = sprintf_s(buffer, "%s" "%s", "Hello", "Hi");
-				int bytesSent = SDLNet_TCP_Send(clientSocket, buffer, sizeof(buffer));
-				if (bytesSent < sizeof(message)) {
-					cerr << "SDLNet TCP Send error: " << SDLNet_GetError() << endl;
-					break;
+			if (std::find(userList.begin(),userList.end(),clientSocket) != userList.end()) {
+				cout << "New User Joined" << endl;
+				std::string bf = Receive(clientSocket,true);
+				string nick = "";
+				if (getInfoType(bf) == infoType::iNickname) {
+					nick = parse(bf, 1);
 				}
+
+				userList.push_back(new User{ nick,clientSocket });
+
+				std::string bufferContent;
+				add(&bufferContent, "msg");
+				add(&bufferContent, "ChArtFX");
+				add(&bufferContent, nick + " joined the chat");
+				for (int i = 0; i < userList.size(); i++)
+				{
+					Send(userList[i]->socket, bufferContent);
+				}
+				
 			}
 			
 			
@@ -83,8 +116,70 @@ int main(int argc, char* argv[]) {
 			}*/
 
 		}
+		for (int i = 0; i < userList.size(); i++)
+		{
+			string bf = Receive(userList[i]->socket, false);
+			if (bf.compare("") == 0) {
+				if (getInfoType(bf) == infoType::iMessage) {
+					for (int j = 0; j < userList.size(); j++)
+					{
+						if (i != j) {
+							Send(userList[j]->socket, bf);
+						}
+					}
+				}
+			}
+		}
 	}
 
 
 	return 0;
+}
+
+void add(std::string* sOut, std::string s2) {
+	sOut->append(s2 + DELIMITER );
+}
+
+std::string parse(std::string s1,int infPos) {
+	size_t pos = 0;
+	int CurrentInfPos = 0;
+	std::string token;
+	while ((pos = s1.find(DELIMITER)) != std::string::npos) {
+		token = s1.substr(0, pos);
+		if (CurrentInfPos == infPos) {
+			return token;
+		}
+		CurrentInfPos++;
+		s1.erase(0, pos + DELIMITER.length());
+	}
+	return "";
+}
+
+infoType getInfoType(std::string s1) {
+	auto it = infoTable.find(parse(s1, 0));
+	if (it != infoTable.end()) {
+		return it->second;
+	}
+	return infoType::Unknown;
+}
+
+void Send(TCPsocket tcp, std::string s) {
+	int bytesSent = SDLNet_TCP_Send(tcp, s.c_str(), s.length() + 1);
+
+	if (bytesSent < s.length() + 1) {
+		cerr << "SDLNet TCP Send error: " << SDLNet_GetError() << endl;
+		return;
+	}
+}
+
+std::string Receive(TCPsocket tcp,bool pause) {
+	SDLNet_SocketSet socketSet = SDLNet_AllocSocketSet(1);
+	SDLNet_AddSocket(socketSet, reinterpret_cast<SDLNet_GenericSocket>(tcp));
+	if (SDLNet_CheckSockets(socketSet, 0) != 0 || pause) {
+		char buffer[1024];
+		int bytesRead = SDLNet_TCP_Recv(tcp, buffer, sizeof(buffer));
+		if (bytesRead > 0) {
+			return std::string(buffer);
+		}
+	}
 }
